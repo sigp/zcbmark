7826a053c73938f1ea9f2bd22dafd2b15238ada2
Paul Hauner <paul@paulhauner.com>
2016-10-07 18:59:00 +1100

    Implement lshw parsing

--- zcbmark.py
+++ zcbmark.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import json
 import statistics
+import pprint
 
 # How many times should the benchmarker repeat the problem
 # We are typically using 20 for this value to get a good
@@ -36,6 +37,47 @@ def average_zcbenchmark_results(zcbenchmark_stdout):
     print('INFO: mean {0}'.format(average))
     return()
 
+def get_lshw_json():
+    proc = subprocess.run(
+            ['lshw -quiet -json'],
+            stdout=subprocess.PIPE,
+            shell=True,
+    )
+    as_string =  proc.stdout.decode('utf-8')
+    as_json = json.loads(as_string)
+    return(as_json)
+
+# recursively loop through the lshw results and return
+# lshw[return_key] when we find an object that matches
+# the find_id
+def find_id_in_lshw_dict(lshw_dict, find_id, return_key):
+    for k, v in lshw_dict.items():
+        if isinstance(v, list):
+            for item in v:
+                result = find_id_in_lshw_dict(item, find_id, return_key)
+                if result:
+                    return result
+        else:
+            if lshw_dict['id'] == find_id:
+                if return_key in lshw_dict:
+                    return lshw_dict[return_key]
+
+# recursively loop through the lshw dict and return some stuff about parents
+def find_memory_in_lshw_dict(lshw_dict, find_class, return_key, parent_id=None):
+    results = []
+    for k, v in lshw_dict.items():
+        if isinstance(v, list):
+            for item in v:
+                result = find_memory_in_lshw_dict(item, find_class, return_key, lshw_dict['id'])
+                if result:
+                    results.append(str(result))
+        else:
+            if lshw_dict['class'] == find_class and parent_id == 'memory':
+                if return_key in lshw_dict:
+                    return lshw_dict[return_key]
+    return '|'.join(results)
+
+
 #
 # Start of Program Execution
 #
@@ -49,7 +91,35 @@ zcash_cli = os.path.join(args.zcash_dir, 'src', 'zcash-cli')
 # initialise an empty dict to store our results
 results = dict()
 
-# proc = subprocess.run(['lshw -short -quiet', '|', 'awk', "'/System Memory '"])
+lshw = get_lshw_json()
+results['product'] = lshw['product']
+results['processor'] = find_id_in_lshw_dict(
+        lshw_dict=lshw,
+        find_id='cpu',
+        return_key='product',
+    )
+results['processor_bits'] = find_id_in_lshw_dict(
+        lshw_dict=lshw,
+        find_id='cpu',
+        return_key='width',
+    )
+results['memory_size'] = find_id_in_lshw_dict(
+        lshw_dict=lshw,
+        find_id='memory',
+        return_key='size',
+    )
+results['memory_descriptions'] = find_memory_in_lshw_dict(
+        lshw_dict=lshw,
+        find_class='memory',
+        return_key='description',
+    )
+results['memory_clocks'] = find_memory_in_lshw_dict(
+        lshw_dict=lshw,
+        find_class='memory',
+        return_key='clock',
+    )
+
+print(results)
 
 # Start at 1 and test all core counts up to the computers amount of cores
 for core_count in range (1, args.cores+1):
